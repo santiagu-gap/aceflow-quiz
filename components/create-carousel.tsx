221050c85bb3fb2fea7a0e5364d9c71b5f7ab4e2
santiagu-gap <santiagu.gap@gmail.com>
2024-02-12 14:04:30 -0500

    Removed limit for free quizzes (set it to 107)

--- components/create-carousel.tsx
+++ components/create-carousel.tsx
@@ -67,7 +67,7 @@ const CreateCarousel = ({ session }: { session: Session | null }) => {
           >
             {session ? (
               <>
-                {session.user.quizzesAnswered < 17 ||
+                {session.user.quizzesAnswered < 107 ||
                 session.user.plan === 'premium' ? (
                   <>
                     <Textarea
